cea1b9ad7d1ad6c32728b9fc47c9231537919747
Haldane Junior <haldanejunior20@gmail.com>
2023-01-03 18:47:50 -0300

    Criei o algoritmo principal

---
+++ CALCULADORA/calculadora-simples.py
@@ -0,0 +1,9 @@
+"""
+    Título: Calculadora simples de somar
+    Autor:  Haldane Junior
+    Data: 03/01/2023
+"""
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite outro numero: '))
+
+print(n1 + n2)
